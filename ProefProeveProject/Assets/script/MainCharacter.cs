e74b2df3c923b70456c048072b48120887b10a12
NikeKamstra <nike_kam@live.nl>
2015-02-09 14:32:10 +0100

    jump on mouseclick

--- ProefProeveProject/Assets/script/MainCharacter.cs
+++ ProefProeveProject/Assets/script/MainCharacter.cs
@@ -10,6 +10,13 @@ public class MainCharacter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		if(Input.GetMouseButtonDown(0)) {
+			rigidbody2D.AddForce(new Vector2(0,100));
+			Debug.Log("down" + Time.deltaTime);
+		} else if(Input.GetMouseButtonUp(0)) {
+			Debug.Log("up" + Time.deltaTime);
+		} else if(Input.GetMouseButton(0)) {
+			Debug.Log("change" + Time.deltaTime);
+		}
 	}
 }
